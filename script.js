9df46b69de3f09f286411f3e0ad900513bc271e1
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-13 00:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const img_urls=["https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80","https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80","https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80","https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80","https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"]
+const texts=["Explore the world","Wild Forest","City on Winter","Mountain Cloud","Sunny Beach"];
+
+const container=document.querySelector(".container");
+img_urls.forEach((url,index)=>{
+	const div=document.createElement("div");
+	div.className="panel";
+	div.id=`panel-${index+1}`;
+	if(index==0){
+		div.classList.add("active");
+	}
+	div.style.backgroundImage=`url(${url})`;
+
+	const textDiv = document.createElement("div");
+    textDiv.className = "img-text";
+    textDiv.innerText = texts[index];
+	div.appendChild(textDiv);
+
+	
+	container.appendChild(div);
+
+	div.addEventListener(("click"),(e)=>{
+		changeAll();
+		div.classList.add("active");
+	})
+})
+function changeAll() {
+	const imgs=document.querySelectorAll(".panel");
+	for(let img of imgs){
+		img.classList.remove("active");
+	}
+}
